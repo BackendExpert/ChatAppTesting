ac624dc2e39842ed2c16661ada93afd77cb57c05
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-13 22:54:35 +0530

    Update UserController.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/Controllers/UserController.js
+++ server/Controllers/UserController.js
@@ -4,25 +4,7 @@ const Contact = require('../Models/Contact')
 const UserController = {
     GetAllUsers: async (req, res) => {
         try{
-            const connections = await Contact.find();
-            const connectionEmails = new Set();
-
-            connections.forEach(conn => {
-                connectionEmails.add(conn.starter);
-                connectionEmails.add(conn.receiver);
-            });
-
-            const nonUserConnection = await User.find({
-                email: { $nin: Array.from(connectionEmails) }
-            });
-
-            if(nonUserConnection){
-                return res.json({ Result: nonUserConnection })
-            }
-            else{
-                return res.json({ Error: "Internel Server Error"})
-            }
-
+            
         }
         catch (err) {
             console.log(err)
