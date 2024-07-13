5d4136d6038338a87831e9ba96d1d6612ba9847f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-13 23:22:11 +0530

    Update ContactRoute.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/Routes/ContactRoute.js
+++ server/Routes/ContactRoute.js
@@ -4,6 +4,6 @@ const ContactController = require('../Controllers/ContactController')
 const router = express.Router()
 
 router.post('/StartContact/:id', ContactController.ContactStart)
-router.get('/MyChats/:id')
+router.get('/MyChats/:id', ContactController.ChatMy)
 
 module.exports = router
