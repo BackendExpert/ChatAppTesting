a27a3a524b3d5d34344de6ef9da8e2233c2adfeb
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-13 19:32:28 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 // app
 
-const authRoute = require('./Routes/UserRoute')
+const authRoute = require('./Routes/authRoute')
 
 const app = express();
 const PORT = process.env.PORT || 5000
@@ -19,6 +19,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/auth', authRoute)
+app.use('/user', )
 
 app.listen(PORT, () => {
     console.log(`Server Running on PORT ${PORT}`)
