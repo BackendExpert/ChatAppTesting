21d93bfa3b785badb8c72a5ea6974f2929661fd6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-13 22:55:53 +0530

    Update UserController.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/Controllers/UserController.js
+++ server/Controllers/UserController.js
@@ -4,7 +4,15 @@ const Contact = require('../Models/Contact')
 const UserController = {
     GetAllUsers: async (req, res) => {
         try{
-            
+            // console.log(req.params.id)
+            const allUser = await User.find({ email: { $ne: req.params.id } })
+
+            if(allUser) {
+                return res.json({ Result: allUser })
+            }
+            else{
+                return res.json({ Error: "Internal Server Error"})
+            }
         }
         catch (err) {
             console.log(err)
