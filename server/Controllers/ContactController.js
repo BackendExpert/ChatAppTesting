3b3bc1954c5fd55f493a2e093abc0810e1605e19
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-13 21:47:59 +0530

    Update ContactController.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/Controllers/ContactController.js
+++ server/Controllers/ContactController.js
@@ -1,7 +1,14 @@
 const Contact = require('../Models/Contact');
 
 const ContactController = {
-
+    ContactStart: async(req, res) => {
+        try{
+            
+        }
+        catch (err) {
+            console.log(err)
+        }
+    }
 }
 
 module.exports = ContactController;
