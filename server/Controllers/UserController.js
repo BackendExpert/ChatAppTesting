24f10d45a2be39cf11699b666d8e85ac277c7a20
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-13 21:06:07 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/Controllers/UserController.js
+++ server/Controllers/UserController.js
@@ -3,15 +3,15 @@ const User = require('../Models/User')
 const UserController = {
     GetAllUsers: async (req, res) => {
         try{
-            console.log(req.params)
-            // const allUser = await User.find()
+            // console.log(req.params.id)
+            const allUser = await User.find({ email: { $ne: req.params.id } })
 
-            // if(allUser) {
-            //     return res.json({ Result: allUser })
-            // }
-            // else{
-            //     return res.json({ Error: "Internal Server Error"})
-            // }
+            if(allUser) {
+                return res.json({ Result: allUser })
+            }
+            else{
+                return res.json({ Error: "Internal Server Error"})
+            }
         }
         catch (err) {
             console.log(err)
