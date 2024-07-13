8ca75f8b5e08c4d448b9b75ef00dbd8ad2327c7b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-13 19:36:41 +0530

    Update UserRoute.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/Routes/UserRoute.js
+++ server/Routes/UserRoute.js
@@ -3,6 +3,6 @@ const UserController = require('../Controllers/UserController')
 
 const router = express.Router()
 
-
+router.get('/GetAllUser', UserController.GetAllUsers)
 
 module.exports = router
