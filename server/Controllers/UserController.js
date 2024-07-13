262a54c55fddac25a1a05f3c1002c31ec2a0a1ae
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-13 19:36:01 +0530

    Update UserController.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/Controllers/UserController.js
+++ server/Controllers/UserController.js
@@ -1,7 +1,14 @@
 const User = require('../Models/User')
 
 const UserController = {
-    
+    GetAllUsers: async (req, res) => {
+        try{
+
+        }
+        catch (err) {
+            console.log(err)
+        }
+    }
 }
 
 module.exports = UserController
