4e3fd819ea8d529d54ded072e5272d71bc8e8b91
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-14 13:36:13 +0530

    Create Message.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ server/Models/Message.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const MessageSchema = new mongoose.Schema({
+    sender: {
+        type: String,
+        required: true,
+    },
+    receiver: {
+        type: String,
+        required: true,
+    },
+    mes: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const Message = mongoose.model('Message', MessageSchema)
+
+module.exports = Message
